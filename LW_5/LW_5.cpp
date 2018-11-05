332d377a124f70c14aa1032677171063cd0ebbab
nikitasa1997 <nikitasa1997@gmail.com>
2018-11-05 21:44:42 +0300

    Add files via upload

--- LW_5/LW_5.cpp
+++ LW_5/LW_5.cpp
@@ -9,32 +9,45 @@
 #include "../generic/header.hpp"
 #include "parabolic_pde.hpp"
 
-static constexpr ldbl u_0_t(const ldbl &, const ldbl &) noexcept;
-static constexpr ldbl u_l_t(const ldbl &, const ldbl &) noexcept;
-static constexpr ldbl u_x_0(const ldbl &, const ldbl &) noexcept;
-static constexpr ldbl u_exact(const ldbl &, const ldbl &, const ldbl &) noexcept;
+static constexpr ldbl u_0_t(const ldbl &, const ldbl &, const ldbl &, const ldbl &) noexcept;
+static constexpr ldbl u_l_t(const ldbl &, const ldbl &, const ldbl &, const ldbl &) noexcept;
+static constexpr ldbl u_x_0(const ldbl &, const ldbl &, const ldbl &, const ldbl &) noexcept;
+static constexpr ldbl u_exact(const ldbl &, const ldbl &, const ldbl &,
+    const ldbl &, const ldbl &) noexcept;
 
 static constexpr ldbl L = PI_LDBL;
-
+static constexpr ldbl ALPHA = 1.0,
+                      BETA  = 1.0,
+                      GAMMA = 1.0,
+                      DELTA = 1.0;
+/*
+static constexpr ldbl L = PI_LDBL;
+static constexpr ldbl ALPHA = 0.0,
+                      BETA  = 1.0,
+                      GAMMA = 0.0,
+                      DELTA = 1.0;
+*/
 int main()
 {
     try
     {
-        ldbl a, t;
+        ldbl a, b, c, t;
         std::size_t n, k;
-        std::cin >> a >> t >> n >> k;
+        std::cin >> a >> b >> c >> t >> n >> k;
         const ublas::vector<ldbl>
-            explicit_fdm_w = explicit_fdm<ldbl>(a, L, n, t, k, u_0_t, u_l_t, u_x_0),
-            implicit_fdm_w = implicit_fdm<ldbl>(a, L, n, t, k, u_0_t, u_l_t, u_x_0),
-            crank_nicolson_w = crank_nicolson<ldbl>(a, L, n, t, k, u_0_t, u_l_t, u_x_0);
+            explicit_fdm_w = explicit_fdm<ldbl>(a, b, c,
+                L, n, t, k, ALPHA, BETA, GAMMA, DELTA, u_0_t, u_l_t, u_x_0),
+            implicit_fdm_w = implicit_fdm<ldbl>(a, b, c,
+                L, n, t, k, ALPHA, BETA, GAMMA, DELTA, u_0_t, u_l_t, u_x_0),
+            crank_nicolson_w = crank_nicolson<ldbl>(a, b, c,
+                L, n, t, k, ALPHA, BETA, GAMMA, DELTA, u_0_t, u_l_t, u_x_0);
         const ldbl h = L / n;
         for (std::size_t i = 0; i < explicit_fdm_w.size(); ++i)
         {
-            std::cout << std::fixed << std::setprecision(9) <<
-                explicit_fdm_w[i] << ' ' << implicit_fdm_w[i] << ' ' <<
-                crank_nicolson_w[i] << ' ' << u_exact(a, i * h, t) << '\n';
+            std::cout << std::fixed << std::setprecision(18) << i * h << ',' <<
+                u_exact(a, b, c, i * h, t) << ',' << explicit_fdm_w[i] << ',' <<
+                implicit_fdm_w[i] << ',' << crank_nicolson_w[i] << '\n';
         }
-        std::cout << '\n';
     } catch (const std::exception &ex)
     {
         std::cout << ex.what() << '\n';
@@ -43,22 +56,45 @@ int main()
     return 0;
 }
 
-static constexpr ldbl u_0_t(const ldbl &a, const ldbl &t) noexcept
+static constexpr ldbl u_0_t(const ldbl &a, const ldbl &b, const ldbl &c, const ldbl &t) noexcept
+{
+    return std::exp((c - a) * t) * (std::cos(b * t) + std::sin(b * t));
+}
+
+static constexpr ldbl u_l_t(const ldbl &a, const ldbl &b, const ldbl &c, const ldbl &t) noexcept
+{
+    return -std::exp((c - a) * t) * (std::cos(b * t) + std::sin(b * t));
+}
+
+static constexpr ldbl u_x_0(const ldbl &, const ldbl &, const ldbl &, const ldbl &x) noexcept
+{
+    return std::sin(x);
+}
+
+static constexpr ldbl u_exact(const ldbl &a, const ldbl &b, const ldbl &c,
+    const ldbl &x, const ldbl &t) noexcept
+{
+    return std::exp((c - a) * t) * std::sin(x + b * t);
+}
+/*
+static constexpr ldbl u_0_t(const ldbl &a, const ldbl &, const ldbl &, const ldbl &t) noexcept
 {
     return std::exp(-a * t);
 }
 
-static constexpr ldbl u_l_t(const ldbl &a, const ldbl &t) noexcept
+static constexpr ldbl u_l_t(const ldbl &a, const ldbl &, const ldbl &, const ldbl &t) noexcept
 {
     return -std::exp(-a * t);
 }
 
-static constexpr ldbl u_x_0(const ldbl &, const ldbl &x) noexcept
+static constexpr ldbl u_x_0(const ldbl &, const ldbl &, const ldbl &, const ldbl &x) noexcept
 {
     return std::cos(x);
 }
 
-static constexpr ldbl u_exact(const ldbl &a, const ldbl &x, const ldbl &t) noexcept
+static constexpr ldbl u_exact(const ldbl &a, const ldbl &, const ldbl &,
+    const ldbl &x, const ldbl &t) noexcept
 {
     return std::exp(-a * t) * std::cos(x);
 }
+*/
--- LW_5/parabolic_pde.hpp
+++ LW_5/parabolic_pde.hpp
@@ -17,31 +17,40 @@ static constexpr std::size_t TWO = 2U;
 
 template<typename T,
     typename = std::enable_if<std::is_floating_point<T>::value>>
-ublas::vector<T> explicit_fdm(const T, const T, std::size_t,
-    const T, std::size_t, const std::function<T (const T &, const T &)> &,
-    const std::function<T (const T &, const T &)> &,
-    const std::function<T (const T &, const T &)> &);
+ublas::vector<T> explicit_fdm(const T, const T, const T,
+    const T, std::size_t, const T, std::size_t,
+    const T, const T, const T, const T,
+    const std::function<T (const T &, const T &, const T &, const T &)> &,
+    const std::function<T (const T &, const T &, const T &, const T &)> &,
+    const std::function<T (const T &, const T &, const T &, const T &)> &);
 
 template<typename T,
     typename = std::enable_if<std::is_floating_point<T>::value>>
-ublas::vector<T> implicit_fdm(const T, const T, std::size_t,
-    const T, std::size_t, const std::function<T (const T &, const T &)> &,
-    const std::function<T (const T &, const T &)> &,
-    const std::function<T (const T &, const T &)> &);
+ublas::vector<T> implicit_fdm(const T, const T, const T,
+    const T, std::size_t, const T, std::size_t,
+    const T, const T, const T, const T,
+    const std::function<T (const T &, const T &, const T &, const T &)> &,
+    const std::function<T (const T &, const T &, const T &, const T &)> &,
+    const std::function<T (const T &, const T &, const T &, const T &)> &);
 
 template<typename T,
     typename = std::enable_if<std::is_floating_point<T>::value>>
-ublas::vector<T> crank_nicolson(const T, const T, std::size_t,
-    const T, std::size_t, const std::function<T (const T &, const T &)> &,
-    const std::function<T (const T &, const T &)> &,
-    const std::function<T (const T &, const T &)> &);
+ublas::vector<T> crank_nicolson(const T, const T, const T,
+    const T, std::size_t, const T, std::size_t,
+    const T, const T, const T, const T,
+    const std::function<T (const T &, const T &, const T &, const T &)> &,
+    const std::function<T (const T &, const T &, const T &, const T &)> &,
+    const std::function<T (const T &, const T &, const T &, const T &)> &);
 
 template<typename T, typename>
-ublas::vector<T> explicit_fdm(const T a, const T l, const std::size_t n_upper,
+ublas::vector<T> explicit_fdm(const T a, const T b, const T c,
+    const T l, const std::size_t n_upper,
     const T t, const std::size_t k_upper,
-    const std::function<T (const T &, const T &)> &u_0_t,
-    const std::function<T (const T &, const T &)> &u_l_t,
-    const std::function<T (const T &, const T &)> &u_x_0)
+    const T alpha, const T beta,
+    const T gamma, const T delta,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_0_t,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_l_t,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_x_0)
 {
     static constexpr T EPSILON = std::numeric_limits<T>::epsilon();
     const T h = l / n_upper, tau = t / k_upper, sigma = a * a * tau / (h * h);
@@ -53,118 +62,121 @@ ublas::vector<T> explicit_fdm(const T a, const T l, const std::size_t n_upper,
     ublas::vector<ublas::vector<T>> w_h_tau(TWO, ublas::vector<T>(n_upper + 1));
     for (std::size_t j = 0; j <= n_upper; ++j)
     {
-        w_h_tau[0][j] = u_x_0(a, j * h);
+        w_h_tau[0][j] = u_x_0(a, b, c, j * h);
     }
     for (std::size_t k = 1; k <= k_upper; ++k)
     {
         ublas::vector<T> &u_k = w_h_tau[k % TWO],
             &u_k_minus_1 = w_h_tau[(k - 1) % TWO];
-        u_k[0]       = u_0_t(a, k * tau);
-        u_k[n_upper] = u_l_t(a, k * tau);
         for (std::size_t j = 1; j < n_upper; ++j)
         {
             u_k[j] = sigma * u_k_minus_1[j + 1] +
                 (1.0 - 2.0 * sigma) * u_k_minus_1[j] +
-                sigma * u_k_minus_1[j - 1];
+                sigma * u_k_minus_1[j - 1] +
+                b * tau * (u_k_minus_1[j + 1] - u_k_minus_1[j - 1]) / (2.0 * h) +
+                c * tau * u_k_minus_1[j];
         }
+        u_k[0] = -alpha / (beta * h - alpha) * u_k[1]
+            + u_0_t(a, b, c, k * tau) * h / (beta * h - alpha);
+        u_k[n_upper] = gamma / (delta * h + gamma) * u_k[n_upper - 1]
+            + u_l_t(a, b, c, k * tau) * h / (delta * h + gamma);
     }
 
     return w_h_tau[k_upper % TWO];
 }
 
 template<typename T, typename>
-ublas::vector<T> implicit_fdm(const T a, const T l, const std::size_t n_upper,
+ublas::vector<T> implicit_fdm(const T a, const T b, const T c,
+    const T l, const std::size_t n_upper,
     const T t, const std::size_t k_upper,
-    const std::function<T (const T &, const T &)> &u_0_t,
-    const std::function<T (const T &, const T &)> &u_l_t,
-    const std::function<T (const T &, const T &)> &u_x_0)
+    const T alpha, const T beta,
+    const T gamma, const T delta,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_0_t,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_l_t,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_x_0)
 {
     static constexpr T EPSILON = std::numeric_limits<T>::epsilon();
     const T h = l / n_upper, tau = t / k_upper, sigma = a * a * tau / (h * h);
-    const std::size_t n_upper_minus_1 = n_upper - 1;
     if (n_upper < 4U || !k_upper || h < EPSILON || tau < EPSILON)
     {
         throw std::logic_error("n < 4 || !k || h < epsilon || tau < epsilon");
     }
 
-    ublas::vector<T> x, d_j(n_upper_minus_1),
-        a_j(n_upper_minus_1, sigma),
-        b_j(n_upper_minus_1, -(1.0 + 2.0 * sigma)),
-        c_j(n_upper_minus_1, sigma);
-    a_j[0] = c_j[n_upper_minus_1 - 1] = 0.0;
+    ublas::vector<T> x, d_j(n_upper + 1),
+        a_j(n_upper + 1, sigma - b * tau / (2.0 * h)),
+        b_j(n_upper + 1, c * tau - (1.0 + 2.0 * sigma)),
+        c_j(n_upper + 1, sigma + b * tau / (2.0 * h));
+    a_j[0] = c_j[n_upper] = 0.0;
+    b_j[0] = beta * h - alpha;
+    c_j[0] = alpha;
+    a_j[n_upper] = -gamma;
+    b_j[n_upper] = gamma + delta * h;
     ublas::vector<ublas::vector<T>> w_h_tau(TWO, ublas::vector<T>(n_upper + 1));
     for (std::size_t j = 0; j <= n_upper; ++j)
     {
-        w_h_tau[0][j] = u_x_0(a, j * h);
+        w_h_tau[0][j] = u_x_0(a, b, c, j * h);
     }
     for (std::size_t k = 1; k <= k_upper; ++k)
     {
         ublas::vector<T> &u_k = w_h_tau[k % TWO],
             &u_k_minus_1 = w_h_tau[(k - 1) % TWO];
-        u_k[0]       = u_0_t(a, k * tau);
-        u_k[n_upper] = u_l_t(a, k * tau);
-        d_j[0] =
-            -(u_k_minus_1[1] + sigma * u_0_t(a, k * tau));
-        d_j[n_upper_minus_1 - 1] =
-            -(u_k_minus_1[n_upper - 1] + sigma * u_l_t(a, k * tau));
-        for (std::size_t j = 1; j < n_upper_minus_1 - 1; ++j)
+        d_j[0] = u_0_t(a, b, c, k * tau) * h;
+        d_j[n_upper] = u_l_t(a, b, c, k * tau) * h;
+        for (std::size_t j = 1; j < n_upper; ++j)
         {
-            d_j[j] = -u_k_minus_1[j + 1];
+            d_j[j] = -u_k_minus_1[j];
         }
-        x = thomas_algorithm(a_j, b_j, c_j, d_j);
-        std::move(x.cbegin(), x.cend(), u_k.begin() + 1);
+        u_k = thomas_algorithm(a_j, b_j, c_j, d_j);
     }
 
     return w_h_tau[k_upper % TWO];
 }
 
 template<typename T, typename>
-ublas::vector<T> crank_nicolson(const T a, const T l, const std::size_t n_upper,
+ublas::vector<T> crank_nicolson(const T a, const T b, const T c,
+    const T l, const std::size_t n_upper,
     const T t, const std::size_t k_upper,
-    const std::function<T (const T &, const T &)> &u_0_t,
-    const std::function<T (const T &, const T &)> &u_l_t,
-    const std::function<T (const T &, const T &)> &u_x_0)
+    const T alpha, const T beta,
+    const T gamma, const T delta,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_0_t,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_l_t,
+    const std::function<T (const T &, const T &, const T &, const T &)> &u_x_0)
 {
     static constexpr T THETA = 0.5, EPSILON = std::numeric_limits<T>::epsilon();
     const T h = l / n_upper, tau = t / k_upper, sigma = a * a * tau / (h * h);
-    const std::size_t n_upper_minus_1 = n_upper - 1;
     if (n_upper < 4U || !k_upper || h < EPSILON || tau < EPSILON)
     {
         throw std::logic_error("n < 4 || !k || h < epsilon || tau < epsilon");
     }
 
-    ublas::vector<T> x, d_j(n_upper_minus_1),
-        a_j(n_upper_minus_1, THETA * sigma),
-        b_j(n_upper_minus_1, -(1.0 + THETA * 2.0 * sigma)),
-        c_j(n_upper_minus_1, THETA * sigma);
-    a_j[0] = c_j[n_upper_minus_1 - 1] = 0.0;
+    ublas::vector<T> x, d_j(n_upper + 1),
+        a_j(n_upper + 1, THETA * (sigma - b * tau / (2.0 * h))),
+        b_j(n_upper + 1, -1.0 + THETA * (c * tau - 2.0 * sigma)),
+        c_j(n_upper + 1, THETA * (sigma + b * tau / (2.0 * h)));
+    a_j[0] = c_j[n_upper] = 0.0;
+    b_j[0] = beta * h - alpha;
+    c_j[0] = alpha;
+    a_j[n_upper] = -gamma;
+    b_j[n_upper] = gamma + delta * h;
     ublas::vector<ublas::vector<T>> w_h_tau(TWO, ublas::vector<T>(n_upper + 1));
     for (std::size_t j = 0; j <= n_upper; ++j)
     {
-        w_h_tau[0][j] = u_x_0(a, j * h);
+        w_h_tau[0][j] = u_x_0(a, b, c, j * h);
     }
     for (std::size_t k = 1; k <= k_upper; ++k)
     {
         ublas::vector<T> &u_k = w_h_tau[k % TWO],
             &u_k_minus_1 = w_h_tau[(k - 1) % TWO];
-        u_k[0]       = u_0_t(a, k * tau);
-        u_k[n_upper] = u_l_t(a, k * tau);
-        d_j[0] =
-            -(u_k_minus_1[1] + THETA * sigma * u_0_t(a, k * tau)) -
-            (1.0 - THETA) * sigma *
-            (u_k_minus_1[2U] - 2.0 * u_k_minus_1[1] + u_k_minus_1[0]);
-        d_j[n_upper_minus_1 - 1] =
-            -(u_k_minus_1[n_upper - 1] + THETA * sigma * u_l_t(a, k * tau)) -
-            (1.0 - THETA) * sigma *
-            (u_k_minus_1[n_upper - 2U] - 2.0 * u_k_minus_1[n_upper - 1] + u_k_minus_1[n_upper]);
-        for (std::size_t j = 1; j < n_upper_minus_1 - 1; ++j)
+        d_j[0] = u_0_t(a, b, c, k * tau) * h;
+        d_j[n_upper] = u_l_t(a, b, c, k * tau) * h;
+        for (std::size_t j = 1; j < n_upper; ++j)
         {
-            d_j[j] = (THETA - 1.0) * sigma *
-                (u_k_minus_1[j + 2U] - 2.0 * u_k_minus_1[j + 1] + u_k_minus_1[j]) -
-                u_k_minus_1[j + 1];
+            d_j[j] = -u_k_minus_1[j] + (THETA - 1.0) *
+                (sigma * (u_k_minus_1[j + 1] - 2.0 * u_k_minus_1[j] + u_k_minus_1[j - 1]) +
+                b * tau / (2.0 * h) * (u_k_minus_1[j + 1] - u_k_minus_1[j - 1]) +
+                c * tau * u_k_minus_1[j]);
         }
-        x = thomas_algorithm(a_j, b_j, c_j, d_j);
-        std::move(x.cbegin(), x.cend(), u_k.begin() + 1);
+        u_k = thomas_algorithm(a_j, b_j, c_j, d_j);
     }
 
     return w_h_tau[k_upper % TWO];
