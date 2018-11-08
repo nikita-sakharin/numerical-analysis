152c0d56cf2d8a7ef6d161d9cd626efd128371a9
nikitasa1997 <nikitasa1997@gmail.com>
2018-11-08 09:58:15 +0300

    Add files via upload

---
+++ generic/header.hpp
@@ -0,0 +1,20 @@
+#ifndef __HEADER_HPP__
+#define __HEADER_HPP__
+
+typedef float flt;
+typedef double dbl;
+typedef long double ldbl;
+
+static constexpr flt
+    PI_FLT = 3.1415926535F,
+    E_FLT  = 2.7182818284F;
+
+static constexpr dbl
+    PI_DBL = 3.141592653589793238,
+    E_DBL  = 2.718281828459045235;
+
+static constexpr ldbl
+    PI_LDBL = 3.1415926535897932384626L,
+    E_LDBL  = 2.7182818284590452353602L;
+
+#endif
